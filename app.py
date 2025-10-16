de542fac1fa468704f8a039d7babe8c0853187f5
Arshiix <164963402+Arshiix@users.noreply.github.com>
2025-10-16 14:33:56 +0100

    Update app.py

--- app.py
+++ app.py
@@ -133,4 +133,5 @@ def api_tools():
 
 if __name__ == '__main__':
     app.wsgi_app = ProxyFix(app.wsgi_app, x_for=1, x_proto=1, x_host=1, x_port=1)
-    
+    port = int(os.environ.get('PORT', 5000))
+    app.run(debug=False, host='0.0.0.0', port=port)
